54715fd2bd9bb312f5b646d4cb5995cf8c0115e7
sarahadeks <sarahopeyemi@yahoo.com>
2023-05-25 15:34:59 -0700

    push and pall

--- 0-push.c
+++ 0-push.c
@@ -55,3 +55,20 @@ void monty_push(stack_t **stack, unsigned int line_number)
 		tmp->next = new;
 	}
 }
+
+/**
+ * monty_pall - Prints the values of a stack_t linked list.
+ * @stack: A pointer to the top mode node of a stack_t linked list.
+ * @line_number: The current working line number of a Monty bytecodes file.
+ */
+void monty_pall(stack_t **stack, unsigned int line_number)
+{
+	stack_t *tmp = (*stack)->next;
+
+	while (tmp)
+	{
+		printf("%d\n", tmp->n);
+		tmp = tmp->next;
+	}
+	(void)line_number;
+}
