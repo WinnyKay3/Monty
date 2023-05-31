fb456b64e3e2a79f42b6726f88e70b91f6947b2d
WinnyKay3 <winffredkalondu@gmail.com>
2023-05-31 11:43:13 -0700

    fix 6-sub.c

--- 6-sub.c
+++ 6-sub.c
@@ -14,7 +14,7 @@ void sub(stack_t **stack, unsigned int line_number)
 
 	if (!stack || !*stack || !((*stack)->next))
 	{
-		fprintf(stderr, "L%d: cant sub, stack too short\n", line_number);
+		fprintf(stderr, "L%d: can't sub, stack too short\n", line_number);
 		exit(EXIT_FAILURE);
 		return;
 	}
