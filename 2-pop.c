6946b4ef5a4ed3815918390773a182e84d30883c
WinnyKay3 <winffredkalondu@gmail.com>
2023-05-31 11:41:37 -0700

    fix 2-pop.c

--- 2-pop.c
+++ 2-pop.c
@@ -19,7 +19,7 @@ void pop(stack_t **stack, unsigned int line_number)
 	}
 	else
 	{
-		fprintf(stderr, "L%u: cant pop an empty stack\n", line_number);
+		fprintf(stderr, "L%u: can't pop an empty stack\n", line_number);
 		exit(EXIT_FAILURE);
 	}
 }
