faabc1ae94df446fb02be3997fa70d82495edf15
WinnyKay3 <winffredkalondu@gmail.com>
2023-05-31 11:45:38 -0700

    fix 9-mod.c

--- 9-mod.c
+++ 9-mod.c
@@ -11,12 +11,12 @@ void mod(stack_t **stack, unsigned int line_number)
 {
 	if (!*stack || !(*stack)->next)
 	{
-		fprintf(stderr, "L%u: can't mod, stack too short", line_number);
+		fprintf(stderr, "L%u: can't mod, stack too short\n", line_number);
 		exit(EXIT_FAILURE);
 	}
 	if ((*stack)->n == 0)
 	{
-		fprintf(stderr, "L%u: divisio n by zero\n", line_number);
+		fprintf(stderr, "L%u: division by zero\n", line_number);
 		exit(EXIT_FAILURE);
 	}
 
